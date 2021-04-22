86fc91b947c8ab3fa5b9b2872f8aaaadf5901fbf
alexander eades <alexander.eades@student.centralia.edu>
2021-04-22 09:21:57 -0700

    Add project files.

---
+++ Eades_Exchanger/ExchangeMonitor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eades_Exchanger
+{
+    class ExchangeMonitor
+    {
+    }
+}
---
+++ Eades_Exchanger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eades_Exchanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
