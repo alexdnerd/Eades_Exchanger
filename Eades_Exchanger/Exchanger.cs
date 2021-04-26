14c7811e099d8904d75942899c7670fbdd693c27
alexander eades <alexander.eades@student.centralia.edu>
2021-04-26 07:58:38 -0700

    math done

--- Eades_Exchanger/Exchanger.cs
+++ Eades_Exchanger/Exchanger.cs
@@ -14,51 +14,63 @@ namespace Eades_Exchanger
         {
             if (currencyChooseInput == "USD" && currencyChooseOutput == "GBP")
             {
-
+                currencyOutput = currencyInput * 0.72523;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "USD" && currencyChooseOutput == "CAN")
             {
-
+                currencyOutput = currencyInput * 1.25427;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "USD" && currencyChooseOutput == "EUR")
             {
-
+                currencyOutput = currencyInput * 0.83572;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "GBP" && currencyChooseOutput == "USD")
             {
-
+                currencyOutput = currencyInput * 1.37887;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "GBP" && currencyChooseOutput == "CAN")
             {
-
+                currencyOutput = currencyInput * 1.72947;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "GBP" && currencyChooseOutput == "EUR")
             {
-
+                currencyOutput = currencyInput * 1.15195;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "CAN" && currencyChooseOutput == "USD")
             {
-
+                currencyOutput = currencyInput * 0.79728;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "CAN" && currencyChooseOutput == "GBP")
             {
-
+                currencyOutput = currencyInput * 0.57848;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "CAN" && currencyChooseOutput == "EUR")
             {
-
+                currencyOutput = currencyInput * 0.66645;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "EUR" && currencyChooseOutput == "USD")
             {
-
+                currencyOutput = currencyInput * 1.19648;
+                return currencyOutput;
             }
             else if (currencyChooseInput == "EUR" && currencyChooseOutput == "GBP")
             {
-
+                currencyOutput = currencyInput * 0.86826;
+                return currencyOutput;
             }
-            else if (currencyChooseInput == "EUR" && currencyChooseOutput == "USD")
+            else if (currencyChooseInput == "EUR" && currencyChooseOutput == "CAN")
             {
-
+                currencyOutput = currencyInput * 1.50040;
+                return currencyOutput;
             }
 
 
