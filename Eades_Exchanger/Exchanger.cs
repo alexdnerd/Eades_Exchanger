6f8f02f20f1356db58b62fc7811112a3bf6d54b5
alexander eades <alexander.eades@student.centralia.edu>
2021-04-26 07:48:14 -0700

    anorter upload

--- Eades_Exchanger/Exchanger.cs
+++ Eades_Exchanger/Exchanger.cs
@@ -4,7 +4,65 @@ using System.Text;
 
 namespace Eades_Exchanger
 {
-    class Exchanger
+    public class Exchanger
     {
+
+        //Quarter = Math.Round(Quarter, 2, MidpointRounding.ToEven);
+        //     Quarter = Math.Floor(Quarter);
+
+        public static double ExchangeMatherizer(string currencyChooseInput, double currencyInput, string currencyChooseOutput, double currencyOutput)
+        {
+            if (currencyChooseInput == "USD" && currencyChooseOutput == "GBP")
+            {
+
+            }
+            else if (currencyChooseInput == "USD" && currencyChooseOutput == "CAN")
+            {
+
+            }
+            else if (currencyChooseInput == "USD" && currencyChooseOutput == "EUR")
+            {
+
+            }
+            else if (currencyChooseInput == "GBP" && currencyChooseOutput == "USD")
+            {
+
+            }
+            else if (currencyChooseInput == "GBP" && currencyChooseOutput == "CAN")
+            {
+
+            }
+            else if (currencyChooseInput == "GBP" && currencyChooseOutput == "EUR")
+            {
+
+            }
+            else if (currencyChooseInput == "CAN" && currencyChooseOutput == "USD")
+            {
+
+            }
+            else if (currencyChooseInput == "CAN" && currencyChooseOutput == "GBP")
+            {
+
+            }
+            else if (currencyChooseInput == "CAN" && currencyChooseOutput == "EUR")
+            {
+
+            }
+            else if (currencyChooseInput == "EUR" && currencyChooseOutput == "USD")
+            {
+
+            }
+            else if (currencyChooseInput == "EUR" && currencyChooseOutput == "GBP")
+            {
+
+            }
+            else if (currencyChooseInput == "EUR" && currencyChooseOutput == "USD")
+            {
+
+            }
+
+
+
+        }
     }
 }
--- Eades_Exchanger/Program.cs
+++ Eades_Exchanger/Program.cs
@@ -6,7 +6,54 @@ namespace Eades_Exchanger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exchanger ExchangeObject = new Exchanger();
+            ExchangeMonitor ExchangeMonObject = new ExchangeMonitor();
+            string currencyChooseInput;
+            double currencyInput;
+            string currencyChooseOutput;
+            double currencyOutput;
+            ConsoleKeyInfo keypress;
+
+            do
+            {
+                Console.WriteLine("Welcome to the Currency Exchanger! Please enter the input currency:");
+                Console.WriteLine("(USD) US Dollar, (GBP) British Pound, (CAN) Canadian Dollar, (EUR) Euro");
+                currencyChooseInput = Console.ReadLine();
+                currencyChooseInput = currencyChooseInput.ToUpper();
+                Console.WriteLine("Please enter the ammount:");
+                currencyInput = double.Parse(Console.ReadLine());
+
+
+                Console.Clear();
+                Console.WriteLine("Please enter the output currency:");
+                Console.WriteLine("(USD) US Dollar, (GBP) British Pound, (CAN) Canadian Dollar, (EUR) Euro");
+                currencyChooseOutput = Console.ReadLine();
+                currencyChooseOutput = currencyChooseOutput.ToUpper();
+
+                Console.Clear();
+                //currencyOutput = ExchangeObject.ExchangeMatherizer(string currencyChooseInput, double currencyInput, string currencyChooseOutput, double currencyOutput);
+
+
+
+
+
+                
+                Console.WriteLine("Would you like to make another exchange?");
+                Console.WriteLine("(E)xchange Again (Q)uit:");
+                keypress = Console.ReadKey();
+
+                if (keypress.KeyChar == 'q')
+                {
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+
+                }
+            } while (keypress.KeyChar != 'q');
+            Console.WriteLine("Thank you for exchanging with us");
         }
     }
 }
